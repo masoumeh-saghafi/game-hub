9603e4d61b0235a3706d377b189dae03e048beb8
masoumeh-saghafi <saghafimasoumeh04@gmail.com>
2024-09-27 23:45:54 +0330

    format code

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,8 +1,7 @@
-import useGenres from "./useGenres";
+import useGenres from './useGenres'
 
 const useGenre = (id?: number) => {
-   const { data: genres } = useGenres()
-return genres.results.find(g => g.id ===id)
-
+  const { data: genres } = useGenres()
+  return genres.results.find(g => g.id === id)
 }
-export default useGenre;
+export default useGenre
